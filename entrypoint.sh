086a9dec3cef982514393bbbb28d91cc1144345b
hau.tran3 <hau.tran3@sotatek.com>
2023-08-03 11:18:36 +0700

    feat: update docker compose

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+./kcn-linux-amd64/bin/kcnd start
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+
+./kcn-linux-amd64/bin/kcnd status
+
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+
+./kcn-linux-amd64/bin/kcnd stop
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
+./kcn-linux-amd64/bin/kcnd status
